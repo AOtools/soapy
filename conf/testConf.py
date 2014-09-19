15d33f3c23ac8dd103996e690cb7eb38d0242af9
Andrew Reeves <a.p.reeves@durham.ac.uk>
2014-09-19 15:06:42 +0100

    added new style configuration

--- conf/testConf.py
+++ conf/testConf.py
@@ -1,40 +1,62 @@
-'''
+"""
 Config File for python tomographic AO simulation.
 
-numpy is imported in the sim, so numpy funcs can be used here.
-'''
+This configuration file contains a python dictionary in which all simulation parameters are saved. The main dictionary ``simConfig``, is split into several sections corresponding to different parts of the simulation, each of which is itself another python dictionary
+
+This file will be parsed and missing parameters will trigger warnings, either defaulting to a reasonable value or exiting the simulation. Python syntax errors in this file can stop the simulation from running, ensure that every pararmeter finishes with a comma, including the sub-dictionaries.
+"""
 import numpy
 
-parameters={
+simConfiguration = {
+
+"Sim":{
     "filePrefix"    :  None,
     "pupilSize"     :   128, #this is the phase SAMPLING size
                             #(not number of detector pxls)
+    "nGS"           :   1,
+    "nDM"           :   1,
+    "nSci"          :   1,
 
-    #Atmosphere Parameters
+    "nIters"        :   1000,
+    "loopTime"      :   1/250.0,
+    "gain"          :   0.6,
+    "aoloopMode"      :   "closed", #Can be "closedSCAO" or "openSCAO" so far...
+    "reconstructor" :   "MVM", #Can be "MVM", "WooferTweeter, LearnAndApply1"
+ 
+    "learnIters"    :   1000, #Only applicable if reconstructor is 
+
+    "saveCMat"      :   False,
+    "saveSlopes"    :   True,
+    "saveDmCommands":   False,
+    "saveLgsPsf"    :   False,
+    "saveSciPsf"    :   True,
+
+    "tipTilt"       :   False,
+    "ttGain"        :   0.9,
+    "wfsMP"            :   False,
+
+    "verbosity"     :   3,
+    },
+
+"Atmosphere":{
     "scrnNo"        :  1,
-    "scrnNames"     :   {   0:"data/phaseScreen_size-4096_r0-10.fits"},
-#                            1:"../screen2.fits",
-#                            2:"../screen3.fits",
-#                            3:"../screen4.fits"
-#                            },
     "scrnHeights"   :   numpy.array([0,5000,10000,25000]),
-    #"scrnStrengths" :   numpy.array([0.20,0.30,0.50,0.50]),
-    "scrnStrengths":   numpy.array([0.5,0.3,0.1,0.1]),
+    "scrnStrengths" :   numpy.array([0.5,0.3,0.1,0.1]),
     "windDirs"      :   numpy.array([0,45,90,135]),
     "windSpeeds"    :   numpy.array([10,10,15,20]),
     "newScreens"    :   True, #Bool
-    "scrnSize"      :   512,
+    "wholeScrnSize" :   512,
     "r0"            :   0.15,
+    },
 
-    
-    #Telescope Parameters
+"Telescope":{
    "telDiam"        :   4.2,  #Metres
    "obs"            :   1.2, #Central Obscuration
    "mask"           :   "circle",
+    },
 
-    #WFS Parameters
-    "GSNo"          :   1,
-    "GSPositions"   :   numpy.array([   [0,0]   ,
+"WFS":{
+    "GSPosition"   :   numpy.array([    [0,0]   ,
                                         [0,-20],
                                         [-20,0],
                                         [20,0],
@@ -48,81 +70,42 @@ parameters={
 
     "subapFOV"      :   numpy.array([4.0]*5), #arcsecs
 
-    "subapOversamp":  numpy.array([4]*5),
-    "waveLengths"   :   numpy.array( [ 600e-9] *5),
-    "subapThreshold":   0.7,
-    "mp_wfs"        :   False,
-    "wfsBitDepth"   :   numpy.array([8]*5),
-    "wfsSNR"        :   numpy.array([0]*5),
+    "subapOversamp" :  numpy.array([4]*5),
+    "wavelength"    :   numpy.array( [ 600e-9] *5),
+    "subapThreshold":   [0.7,]*5,
+    "bitDepth"      :   numpy.array([8]*5),
+    "SNR"           :   numpy.array([0]*5),
     "removeTT"      :   numpy.array([0,0,0,0,0]),
     "angleEquivNoise":  numpy.array([0]*5), #arcsecs
+    },
 
-    #LGS Parameter
-    "LGSPupilSize"  :   0.5, #Metres
-    "LGSUplink"     :   numpy.array([   0 ]*5),
-    "LGSFFTPadding" :   numpy.array([   128     ]*5),
-    "LGSWL"         :   numpy.array([   600e-9  ]*5),
-    "lgsPropMethod" :   ["physical"]*5,
-    "lgsHeight"     :   numpy.array([0]*5),
-    "lgsElongation" :   0,
-    "elongLayers"   :   10,
+"LGS":{
+    "lgsPupilSize"  :   0.5, #Metres
+    "lgsUplink"     :   numpy.array([   1 ]*5),
+    "wavelength"         :   numpy.array([   600e-9  ]*5),
+    "propogationMode" :   ["physical"]*5,
+    "height"     :   numpy.array([90000]*5),
+    "elongationDepth" :   [0,]*5,
+    "elongationLayers"   :   [10,]*5,
+    },
 
-    #DM Parameter9
-    "tipTilt"       :   False,
-    "ttGain"        :   0.9,
-    "dmNo"          :   1,
-    "dmTypes"        :   [ "peizo","peizo"], #peizo,TT or zernike so far...
+"DM":{
+
+    "dmType"        :   [ "peizo","peizo"], #peizo,TT or zernike so far...
     "dmActs"        :   numpy.array( [9**2,33**2]),
     "dmCond"        :   numpy.array( [0.05, 0.05] ),
+    },
 
-
-    #Science Parameters
-    "sciNo"         : 1,
-    "sciPos"        :   numpy.array( [ [0,0], [0,0],[0,0],[0,0] ] ),
-    "sciFOV"        :   numpy.array( [3.0]*4), #Arcsecs
-    "sciWvls"       :   numpy.array( [ 1500e-9, 800e-9, 1000e-9, 1500e-9]),
-    "sciPxls"       :   numpy.array( [128]*4),
-    "sciOversamp"   :   numpy.array( [4]*4 ),
-
-    #Loop Parameters
-    "nIters"        :   1000,
-    "loopTime"      :   1/250.0,
-    "gain"          :   0.6,
-    "loopMode"      :   "closed", #Can be "closedSCAO" or "openSCAO" so far...
-    "reconstructor" :   "MVM", #Can be "MVM", "WooferTweeter, LearnAndApply1"
-    
-    #L&A Params  
-    "learnIters"    :   1000, #Only applicable if reconstructor is "LearnAndApply"
-    "LAscrnNo"        :  4,
-    "LAscrnNames"     :   {   0:"data/phaseScreen_size-4096_r0-10.fits"},
-#                            1:"../screen2.fits",
-#                            2:"../screen3.fits",
-#                            3:"../screen4.fits"
-#                            },
-    "LAscrnHeights"   :   numpy.array([0,5000,10000,25000]),
-    #"scrnStrengths" :   numpy.array([0.20,0.30,0.50,0.50]),
-    "LAscrnStrengths":   numpy.array([0.5,0.3,0.1,0.1]),
-    "LAwindDirs"      :   numpy.array([0,45,90,135]),
-    "LAwindSpeeds"    :   numpy.array([10,10,15,20]),
-    "LAnewScreens"    :   True, #Bool
-    "LAscrnSize"      :   1024,
-    "LAr0"            :   0.15,
-    
-
-
-    #Debugging
-    "logging"       :   1,
-
-    #Data Saving
-    "saveCMat"      :   False,
-    "saveSlopes"    :   True,
-    "saveDmCommands":   False,
-    "saveLgsPsf"    :   False,
-    "saveSciPsf"    :   True,
-
+"Science":{
+    "position"        :   numpy.array( [ [0,0], [0,0],[0,0],[0,0] ] ),
+    "FOV"        :   numpy.array( [3.0]*4), #Arcsecs
+    "wavelength"       :   numpy.array( [ 1500e-9, 800e-9, 1000e-9, 1500e-9]),
+    "pxls"       :   numpy.array( [128]*4),
+    "oversamp"   :   numpy.array( [4]*4 ),
     }
 
 
+}
 
 
 
