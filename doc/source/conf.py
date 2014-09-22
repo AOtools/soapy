39d78c987d9b05308ceccdc4433513890087e6b0
andrewpaulreeves <a.p.reeves@durham.ac.uk>
2014-09-22 11:49:34 +0100

    Update conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -21,7 +21,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['scipy', 'pyfftw', 'ipython', 'scipy.fftpack','scipy.interpolate', 'pyfits', 'PyQt4','scipy.ndimage']
+MOCK_MODULES = ['scipy', 'pyfftw', 'ipython', 'scipy.fftpack','scipy.interpolate', 'pyfits', 'PyQt4','scipy.ndimage','scipy.optimize']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
