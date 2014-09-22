754734572ecac35396b108bbd3d58fbeb9979eb3
andrewpaulreeves <a.p.reeves@durham.ac.uk>
2014-09-22 17:02:03 +0100

    Update conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -25,7 +25,7 @@ class Mock(MagicMock):
 MOCK_MODULES = ['pyfftw', 'ipython','pyfits', 'PyQt4','IPython.qt.console.rich_ipython_widget',
                     'IPython.qt.inprocess', 'IPython.lib', 'IPython.lib',
                     'matplotlib.backends.backend_qt4agg', 'sip', 'pyqtgraph','pylab', 'OpenGL',
-                    'matplotlib.figure']
+                    'matplotlib.figure','IPython.qt.console.rich_ipython_widget']
 #'scipy.ndimage','scipy.optimize', 'scipy.lib.blas.fblas','scipy.fftpack','scipy.interpolate','scipy']
 
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
