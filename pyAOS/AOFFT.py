a4f20eb31d94beeb725a927213f47a85f09a6117
Andrew Reeves <a.p.reeves@durham.ac.uk>
2014-09-24 08:57:29 +0100

    added more copyright notices, removed some uneeded files

--- pyAOS/AOFFT.py
+++ pyAOS/AOFFT.py
@@ -1,3 +1,21 @@
+#Copyright Durham University and Andrew Reeves
+#2014
+
+# This file is part of pyAOS.
+
+#     pyAOS is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+
+#     pyAOS is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+
+#     You should have received a copy of the GNU General Public License
+#     along with pyAOS.  If not, see <http://www.gnu.org/licenses/>.
+
 '''
 A Module to perform FFTs, wrapping a variety of FFT Backends in a common
 interface.
