b617e5b545a6e9c9bd60e8c7095a2560c607c1e6
Andrew Reeves <a.p.reeves@durham.ac.uk>
2014-11-26 14:01:32 +0000

    changed setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
 from distutils.core import setup
+import versioneer
 
+
+versioneer.VCS = 'git'
+versioneer.versionfile_source = 'pyAOS/_version.py'
+versioneer.versionfile_build = 'pyAOS/_version.py'
+versioneer.tag_prefix = '' # tags are like 1.2.0
+versioneer.parentdir_prefix = 'pyAOS-' # dirname like 'myproject-1.2.0'
+  
+  
 setup(
     name='pyAOS',
-    version='0.1.0',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     author='Andrew Reeves',
     author_email='a.p.reeves@durham.ac.uk',
     packages=['pyAOS', 'pyAOS'],
