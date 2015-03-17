ea57c5c149c2bb7b120c38d352cf44e8e7b6fbd0
Andrew Reeves <a.p.reeves@durham.ac.uk>
2015-03-17 09:15:28 +0000

    Added a multi-threaded version of 2d interpolation
    This uses the numba ``nogil`` keyword to implement *real*
    shared memory paralisation, acheiving close to the optimal speedup

--- pyAOS/aoSimLib.py
+++ pyAOS/aoSimLib.py
@@ -308,6 +308,7 @@ def linterp2d_numba(data, xCoords, yCoords, interpArray):
 
             yGrad = a2 - a1
             interpArray[i,j] = a1 + yGrad*(y-y1)
+    return interpArray
     
 
 @jit(nopython=True)
@@ -343,6 +344,81 @@ def zoom_numba(data, zoomArray):
 
 
     return zoomArray
+
+def threadInterp(data, xCoords, yCoords, interpArray, threads):
+    """
+    A function which deals with threaded numba interpolation.
+
+    Parameters:
+        array (ndarray): The 2-D array to interpolate
+        xCoords (ndarray): A 1-D array of x-coordinates 
+        yCoords (ndarray): A 2-D array of y-coordinates
+        interpArray (ndarray): The array to place the calculation
+        threads (int): Number of threads to use for calculation
+
+    Returns:
+        interpArray (ndarray): A pointer to the calculated ``interpArray''
+    """
+
+    nx = xCoords.shape[0]
+
+    Ts = []
+    for t in range(threads):
+        Ts.append(Thread(target=linterp2d_numbaThread, 
+            args = (
+                data, xCoords, yCoords, 
+                numpy.array([int(t*nx/threads), int((t+1)*nx/threads)]),
+                interpArray)
+            ))
+        Ts[t].start()
+
+    for T in Ts:
+        T.join()
+
+    return interpArray
+
+@numba.jit(nopython=True, nogil=True)
+def linterp2d_numbaThread(data, xCoords, yCoords, chunkIndices, interpArray):
+    """
+    2-D interpolation using purely python - fast if compiled with numba
+    This version also accepts a parameter specifying how much of the array
+    to operate on. This is useful for multi-threading applications.
+
+    Parameters:
+        array (ndarray): The 2-D array to interpolate
+        xCoords (ndarray): A 1-D array of x-coordinates 
+        yCoords (ndarray): A 2-D array of y-coordinates
+        chunkIndices (ndarray): A 2 element array, with (start Index, stop Index) to work on for the x-dimension.
+        interpArray (ndarray): The array to place the calculation
+
+    Returns:
+        interpArray (ndarray): A pointer to the calculated ``interpArray''
+    """
+
+
+    if xCoords[-1] == data.shape[0]-1:
+        xCoords[-1] -= 1e-6
+    if yCoords[-1] == data.shape[1]-1:
+        yCoords[-1] -= 1e-6
+
+    jRange = xrange(yCoords.shape[0])
+    for i in xrange(chunkIndices[0],chunkIndices[1]):
+        x = xCoords[i]
+        x1 = numba.int32(x)
+        for j in jRange: 
+            y = yCoords[j] 
+            y1 = numba.int32(y)
+
+            xGrad1 = data[x1+1, y1] - data[x1, y1]
+            a1 = data[x1, y1] + xGrad1*(x-x1)
+
+            xGrad2 = data[x1+1, y1+1] - data[x1, y1+1]
+            a2 = data[x1, y1+1] + xGrad2*(x-x1)
+
+            yGrad = a2 - a1
+            interpArray[i,j] = a1 + yGrad*(y-y1)
+
+    return interpArray
 
 
 def interp2d_numpy(array, xCoords, yCoords, interpArray=None):
