26b31e816779da2105f7a8fe8342c08da8b3151f
Andrew Reeves <a.p.reeves@durham.ac.uk>
2015-04-03 22:06:30 +0100

    added more tests

--- test/testSimulation.py
+++ test/testSimulation.py
@@ -1,21 +1,92 @@
 import unittest
 
 import pyAOS
+import numpy
+
+RESULTS = {
+        "8x8": 0.76,
+        "8x8_offAxis": 0.30,
+        "8x8_zernike": 0.65,
+        }
+
 
 class TestSimpleSCAO(unittest.TestCase):
 
-    def testAOInit(self):
+    def testOnAxis(self):
         sim = pyAOS.Sim("../conf/sh_8x8.py")
         sim.config.sim.filePrefix = None
         sim.config.sim.logfile = None
         sim.config.sim.nIters = 100
+        sim.config.wfs[0].GSPosition=(0,0)
+        
         sim.aoinit()
 
-    #def testIMat(self):
         sim.makeIMat(forceNew=True)
 
-    #def testLoop(self):
         sim.aoloop()
+
+        #Check results are ok
+        assert numpy.allclose(sim.longStrehl[0,-1], RESULTS["8x8"], atol=0.2)
+
+
+    def testOffAxis(self):
+        sim = pyAOS.Sim("../conf/sh_8x8.py")
+        sim.config.sim.filePrefix = None
+        sim.config.sim.logfile = None
+        sim.config.sim.nIters = 100
+        sim.config.wfs[0].GSPosition = (20,0)
+        
+        sim.aoinit()
+
+        sim.makeIMat(forceNew=True)
+
+        sim.aoloop()
+
+        #Check results are ok
+        assert numpy.allclose(
+                sim.longStrehl[0,-1], RESULTS["8x8_offAxis"], atol=0.2)
+
+    
+    def testZernikeDM(self):
+        sim = pyAOS.Sim("../conf/sh_8x8.py")
+        sim.config.sim.filePrefix = None
+        sim.config.sim.logfile = None
+        sim.config.sim.nIters = 100
+        sim.config.wfs[0].GSPosition = (0,0)
+        
+        sim.config.sim.nDM = 1
+        sim.config.dm[0].dmType = "Zernike"
+        sim.config.dm[0].dmActs = 45
+        sim.config.dm[0].dmCond = 0.01
+        
+        sim.aoinit()
+
+        sim.makeIMat(forceNew=True)
+
+        sim.aoloop()
+
+        #Check results are ok
+        assert numpy.allclose(
+                sim.longStrehl[0,-1], RESULTS["8x8_zernike"], atol=0.2)
+
+
+class TestLgsAO:
+
+    def testElongation(self):
+
+        sim = pyAOS.Sim("../conf/sh_8x8_lgs_elongation.py")
+        
+
+        sim.aoinit()
+
+        sim.makeIMat(forceNew=True)
+
+        sim.aoloop()
+
+        #Check results are ok
+        assert numpy.allclose(
+                sim.longStrehl[0,-1], RESULTS["8x8_zernike"], atol=0.2)
+
 
 if __name__ == '__main__':
     unittest.main()
