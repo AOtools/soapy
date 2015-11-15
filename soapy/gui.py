4e3dbc8e7dc1f30ce0a88927a9594d90f9a75d9a
Andrew Reeves <andrewpaulreeves@gmail.com>
2015-11-15 18:35:13 +0000

    remove debug prints

--- soapy/gui.py
+++ soapy/gui.py
@@ -258,7 +258,6 @@ class GUI(QtGui.QMainWindow):
 
                 if numpy.any(plotDict["wfsPhase"][wfs])!=None:
                     wfsPhase = plotDict["wfsPhase"][wfs]
-                    print("WFS Phase:{}".format(wfsPhase.max()))
                     self.phasePlots[wfs].setImage(
                             wfsPhase, lut=self.LUT, levels=scaleValues)
 
@@ -273,7 +272,6 @@ class GUI(QtGui.QMainWindow):
             for dm in range(self.config.sim.nDM):
                 if numpy.any(plotDict["dmShape"][dm]) !=None:
                     dmShape = plotDict["dmShape"][dm]
-                    print("DM Shape:{}".format(dmShape.max()))
                     self.dmPlots[dm].setImage(plotDict["dmShape"][dm],
                                             lut=self.LUT, levels=scaleValues)
 
