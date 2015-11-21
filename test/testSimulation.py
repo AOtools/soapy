734a1203633c8322b5e100ddb499429dfd1783cd
Andrew Reeves <andrewpaulreeves@gmail.com>
2015-11-21 20:55:03 +0000

    added open loop test. Add test coverage

--- test/testSimulation.py
+++ test/testSimulation.py
@@ -79,6 +79,7 @@ class TestSimpleSCAO(unittest.TestCase):
         sim.config.dms[0].type = "Zernike"
         sim.config.dms[0].nxActuators = 45
         sim.config.dms[0].svdConditioning = 0.01
+        sim.config.dms[0].iMatValue=100
         
         sim.aoinit()
 
@@ -108,6 +109,25 @@ class TestSimpleSCAO(unittest.TestCase):
         #Check results are ok
         assert numpy.allclose(
                 sim.longStrehl[0,-1], RESULTS["8x8_lgs"], atol=0.2)
+
+    def testOpenLoop(self):
+        sim = soapy.Sim("../conf/sh_8x8.py")
+        sim.config.sim.simName = None
+        sim.config.sim.logfile = None
+        sim.config.sim.nIters = 100
+        sim.config.wfss[0].GSPosition=(0,0)
+
+        for i in range(sim.config.sim.nDM-1):
+            sim.config.dms[i+1].closed = False
+
+        sim.aoinit()
+
+        sim.makeIMat(forceNew=True)
+
+        sim.aoloop()
+
+        #Check results are ok
+        assert numpy.allclose(sim.longStrehl[0,-1], RESULTS["8x8"], atol=0.2)
 
 
 if __name__ == '__main__':
