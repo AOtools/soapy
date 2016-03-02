6253f93f6a9df6b503c6ade70ee4e2a633efdd46
Matthew Townson <matthew.townson@durham.ac.uk>
2016-03-02 20:05:00 +0000

    fix correlation wfs in new wfs structure

--- conf/sh_8x8_corr.py
+++ conf/sh_8x8_corr.py
@@ -12,13 +12,13 @@ simConfiguration = {
 "Sim":{
     "simName"       :  "sh_8x8",
     "logfile"       :   "sh_8x8.log",
-    "pupilSize"     :   128, 
+    "pupilSize"     :   128,
     "nGS"           :   1,
     "nDM"           :   2,
     "nSci"          :   1,
     "nIters"        :   5000,
     "loopTime"      :   1/400.0,
-    "reconstructor" :   "MVM", 
+    "reconstructor" :   "MVM",
 
     "verbosity"     :   2,
 
@@ -54,8 +54,8 @@ simConfiguration = {
     "subapFOV"      :   [2.5],
     "fftOversamp"   :   [3],
     "wavelength"    :   [600e-9],
-    "centMethod"    :   ["correlation"],
-    "referenceImage":   ["refImNat.fits"],
+    "centMethod"    :   ["centreOfGravity"],
+    "referenceImage":   ["refImNat.fits"],      # Doesn't work, need full path
     "centThreshold" :   [0.1]
     },
 
@@ -80,6 +80,3 @@ simConfiguration = {
     "fftOversamp"      :   [2],
     }
 }
-
-
-
--- soapy/confParse.py
+++ soapy/confParse.py
@@ -527,6 +527,8 @@ class WfsConfig(ConfigObj):
                             set to loopTime.                    ``None``
         ``wvlBandWidth``    float: Width of wavelength
                             band sent to WFS in nm              ``100``
+        ``extendedObject``  ndarray or str: The object used
+                            as extended source for WFS.         ``None``
         ``fftwThreads``     int: number of threads for fftw
                             to use. If ``0``, will use
                             system processor number.           ``1``
@@ -570,6 +572,7 @@ class WfsConfig(ConfigObj):
                                 ("photonNoise", False),
                                 ("GSMag", 0.0),
                                 ("wvlBandWidth", 100.),
+                                ("extendedObject", None)
                             ]
         self.initParams()
 
