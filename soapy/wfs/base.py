9201c1fa22503bde5d4ea159f34995bb583c6f7d
Andrew Reeves <andrewpaulreeves@gmail.com>
2016-06-22 16:59:10 +0100

    Only set Nan subap to zero if nans found

--- soapy/wfs/base.py
+++ soapy/wfs/base.py
@@ -448,8 +448,10 @@ class WFS(object):
             self.config.eReadNoise = eReadNoise
 
         # Check that slopes aint `nan`s. Set to 0 if so
+        # if numpy.any(numpy.isnan(self.slopes)):
+        #     self.slopes[:] = 0
         if numpy.any(numpy.isnan(self.slopes)):
-            self.slopes[:] = 0
+            numpy.nan_to_num(self.slopes)
 
         return self.slopes
 
