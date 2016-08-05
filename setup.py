992ee4a45d328427a271be82a1ba5fddc39a1fec
Matthew Townson <matthew.townson@durham.ac.uk>
2016-08-05 10:35:49 +0100

    found bug in setup not including all of aotools

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ import versioneer
 versioneer.VCS = 'git'
 versioneer.versionfile_source = 'soapy/_version.py'
 versioneer.versionfile_build = 'soapy/_version.py'
-versioneer.tag_prefix = '' # tags are like 1.2.0
-versioneer.parentdir_prefix = 'soapy-' # dirname like 'myproject-1.2.0'
+versioneer.tag_prefix = ''  # tags are like 1.2.0
+versioneer.parentdir_prefix = 'soapy-'  # dirname like 'myproject-1.2.0'
   
   
 setup(
@@ -15,13 +15,15 @@ setup(
     cmdclass=versioneer.get_cmdclass(),
     author='Andrew Reeves',
     author_email='a.p.reeves@durham.ac.uk',
-    packages=['soapy', 'soapy.wfs', 'soapy.aotools'],
+    packages=['soapy', 'soapy.wfs', 'soapy.aotools', 'soapy.aotools.centroiders', 'soapy.aotools.circle',
+              'soapy.aotools.fft', 'soapy.aotools.interp', 'soapy.aotools.interp', 'soapy.aotools.phasescreen',
+              'soapy.aotools.wfs'],
     scripts=['bin/soapy'],
     description='A tomographic astronomical adaptive optics simulation with realistic laser guide star propagation.',
     long_description=open('README.md').read(),
     install_requires=[
        "numpy >= 1.7.0",
        "scipy >= 0.15.0",
-      "astropy >= 1.0",
+       "astropy >= 1.0",
       ],
 )
