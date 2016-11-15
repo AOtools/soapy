8a9da4f159cf00e32660c5eafc2e79f173944f74
Andrew Reeves <andrewpaulreeves@gmail.com>
2016-11-15 17:59:54 +0000

    Update AOtools

--- soapy/aotools/aotools.py
+++ soapy/aotools/aotools.py
@@ -30,3 +30,36 @@ def photonsPerMag(mag, mask, pxlScale, wvlBand, expTime):
     photons = photonPerSec * expTime
 
     return photons
+
+def image_contrast(image):
+    """
+    Calculates the 'Michelson' contrast.
+
+    Uses a method by Michelson (Michelson, A. (1927). Studies in Optics. U. of Chicago Press.), to calculate the contrast ratio of an image. Uses the formula:
+        (img_max - img_min)/(img_max + img_min)
+
+    Parameters:
+        image (ndarray): Image array
+ 
+    Returns:
+        float: Contrast value
+    """
+
+    contrast = (image.max() - image.min()) / (image.max() + image.min())
+
+    return contrast
+
+def rms_contrast(image):
+    """
+    Calculates the RMS contrast - basically the standard deviation of the image
+
+    Parameters:
+        image (ndarray): Image array
+
+    Returns:
+        float: Contrast value
+    """
+    
+    image /= image.max()
+    
+    return image.std()
