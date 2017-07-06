9883ec6504b346e41b816893c9dcc0c4aa02b382
Andrew Reeves <a.p.reeves@durham.ac.uk>
2017-07-06 15:00:03 +0100

    Add hack to stop crashes on Windows ctrl-c to soapy __init__

--- soapy/__init__.py
+++ soapy/__init__.py
@@ -18,6 +18,34 @@
 #     You should have received a copy of the GNU General Public License
 #     along with soapy.  If not, see <http://www.gnu.org/licenses/>.
 
+import os
+import sys
+if sys.platform == "win32":
+    # hack to stop the interpretter craching on ctrl-c events when scipy imported.
+    # described in more detail here:
+    # http://stackoverflow.com/questions/15457786/ctrl-c-crashes-python-after-importing-scipy-stats
+
+    import imp
+    import ctypes
+    import _thread as thread
+    import win32api
+
+    # Load the DLL manually to ensure its handler gets
+    # set before our handler.
+    basepath = os.path.dirname(sys.executable)
+    ctypes.CDLL(os.path.join(basepath, "Library", "bin", 'libmmd.dll'))
+    ctypes.CDLL(os.path.join(basepath, "Library", "bin", 'libifcoremd.dll'))
+
+    # Now set our handler for CTRL_C_EVENT. Other control event
+    # types will chain to the next handler.
+    def handler(dwCtrlType, hook_sigint=thread.interrupt_main):
+        if dwCtrlType == 0: # CTRL_C_EVENT
+            hook_sigint()
+            return 1 # don't chain to the next handler
+        return 0 # chain to the next handler
+
+    win32api.SetConsoleCtrlHandler(handler, 1)
+
 # Useful things to have in top namespace
 from .simulation import Sim, make_mask
 from .confParse import loadSoapyConfig
